4739526461659fd5993298ebecf8ff8ad1d6c7b8
remi-kn <remi.blandin@gmail.com>
2021-10-14 17:55:24 +0200

    Update validation data

--- validation/elephant_trunk/elephant_compare_FEM_MM.m
+++ validation/elephant_trunk/elephant_compare_FEM_MM.m
@@ -14,15 +14,29 @@ l = 0.25;
 fac = (10^5)/2/pi;
 
 figure
+subplot(2,1,1)
 hold on
-plot(f, 20*log10(abs(Hrb)))
-plot(f, 20*log10(abs(H)))
-plot(elephant_ac_press_MM(:,1), 20*log10(fac*elephant_ac_press_MM(:,2)))
+plot(f, 20*log10(abs(Hrb)), 'linewidth', 2)
+plot(f, 20*log10(abs(H)), 'linewidth', 2)
+plot(elephant_ac_press_MM(:,1), 20*log10(elephant_ac_press_MM(:,2)), 'linewidth', 2)
 xlim([0 10000])
 ylim([80 120])
 xlabel('f (Hz)')
 ylabel('|H| (dB)')
-legend('FEM rad Big', 'FEM rad small', 'MM', 'location', 'southeast')
+h = legend('FEM rad Big', 'FEM rad small', 'MM', 'location', 'southeast');
+set(h, 'fontsize', ftSize);
+set(gca, 'fontsize', ftSize);
+
+subplot(2,1,2)
+hold on
+plot(f, unwrap(-angle(Hrb)), 'linewidth', 2)
+plot(f, unwrap(-angle(H)), 'linewidth', 2)
+plot(elephant_ac_press_MM(:,1), unwrap(elephant_ac_press_MM(:,3)), 'linewidth', 2)
+xlim([0 10000])
+xlabel('f (Hz)')
+ylabel('phase (deg)')
+
+set(gca, 'fontsize', ftSize);
 
 
 %% zero pressure at the opening
@@ -35,15 +49,27 @@ load elephant_ac_press_MM_p0.txt
 fac = input_area*(10^5);
 
 figure
+subplot(2,1,1)
 hold on
 plot(f, 20*log10(abs(Hp0_orig)), 'k', 'linewidth', 2)
 plot(f, 20*log10(abs(H)), 'r', 'linewidth', 2)
-plot(elephant_ac_press_MM_p0(:,1), 20*log10(fac*elephant_ac_press_MM_p0(:,2)), 'b', 'linewidth', 2)
+plot(elephant_ac_press_MM_p0(:,1), 20*log10(elephant_ac_press_MM_p0(:,2)), 'b', 'linewidth', 2)
 
 xlim([0 10000])
 ylim([70 140])
 xlabel('f (Hz)')
 ylabel('|H| (dB)')
-h = legend('FEM original','FEM stabilized', 'MM', 'location', 'southeast');
+h = legend('FEM original','FEM stabilized', 'MM', 'location', 'southeast', 'orientation', 'horizontal');
 set(h, 'fontsize', ftSize);
+set(gca, 'fontsize', ftSize);
+
+subplot(2,1,2)
+hold on
+plot(f, -angle(Hp0_orig), 'k', 'linewidth', 2)
+plot(f, -angle(H), 'r', 'linewidth', 2)
+plot(elephant_ac_press_MM_p0(:,1), elephant_ac_press_MM_p0(:,3), 'b', 'linewidth', 2)
+
+xlim([0 10000])
+xlabel('f (Hz)')
+ylabel('phase (deg)')
 set(gca, 'fontsize', ftSize);
--- validation/junction_simple_cones/plot_tfs_junction_simple_cones.m
+++ validation/junction_simple_cones/plot_tfs_junction_simple_cones.m
@@ -1,5 +1,11 @@
-%load tfMM_2_1_1_05.txt;
-%tf2 = tfMM_2_1_1_05;
+load tfMM_2_1_1_05.txt;
+tf1 = tfMM_2_1_1_05;
+load tfMM_2_1_2_1.txt;
+tf2 = tfMM_2_1_2_1;
+load tfMM_1_05_1_05.txt;
+tf3 = tfMM_1_05_1_05;
+load tfMM_1_05_2_1.txt;
+tf4 = tfMM_1_05_2_1;
 %load tfFEM.txt;
 %tf3 = tfFEM;
 load data_junction_simple_cones.mat
@@ -9,31 +15,41 @@ ftSize = 16;
 
 figure
 subplot(2,1,1)
-plot(tfMM(:,1), 20*log10(fac*tfMM(:,2)), 'lineWidth', 2)
 hold on
-%plot(tf2(:,1), 20*log10(4*fac*tf2(:,2)), 'lineWidth', 2)
-plot(tfFEM(:,1), 20*log10(tfFEM(:,2)), 'lineWidth', 2)
-%plot(tf3(:,1), 20*log10(tf3(:,2)), 'lineWidth', 2)
+%plot(tfFEM(:,1), 20*log10(tfFEM(:,2)), 'lineWidth', 2)
+%plot(tfMM(:,1), 20*log10(fac*tfMM(:,2)), 'lineWidth', 2)
+plot(tf1(:,1), 20*log10(tf1(:,2)), 'lineWidth', 2)
+plot(tf2(:,1), 20*log10(tf2(:,2)), 'lineWidth', 2)
+plot(tf3(:,1), 20*log10(tf3(:,2)), 'lineWidth', 2)
+plot(tf4(:,1), 20*log10(tf4(:,2)), 'lineWidth', 2)
 
 xlim([0 10000])
 xlabel 'f (Hz)'
 ylabel '|H| (dB)'
 
-h = legend('MM', 'FEM (FeniCS)', 'location', 'northeast',...
-  'orientation', 'horizontal')
-legend boxoff
-set(h, 'fontSize', ftSize) 
+%h = legend('FEM (FeniCS)', 'MM', 'location', 'northeast',...
+  %'orientation', 'horizontal')
+%legend boxoff
+%set(h, 'fontSize', ftSize) 
 set(gca, 'fontSize', ftSize)
 
 subplot(2,1,2)
-plot(tfMM(:,1), tfMM(:,3), 'lineWidth', 2)
 hold on
-plot(tfFEM(:,1), tfFEM(:,3), 'lineWidth', 2)
+%plot(tfFEM(:,1), tfFEM(:,3), 'lineWidth', 2)
+%plot(tfMM(:,1), tfMM(:,3), 'lineWidth', 2)
+plot(tf1(:,1), tf1(:,3), 'lineWidth', 2)
+plot(tf2(:,1), tf2(:,3), 'lineWidth', 2)
+plot(tf3(:,1), tf3(:,3), 'lineWidth', 2)
+plot(tf4(:,1), tf4(:,3), 'lineWidth', 2)
 
 xlim([0 10000])
 xlabel 'f (Hz)'
 ylabel '|H| (dB)'
 
+h = legend('|2 | 1 | 1 | 0.5|', '|2 | 1 | 2 | 1|', '|1 | 0.5 | 1 | 0.5|', '|1 | 0.5 | 2 | 1|',...
+  'location', 'northoutside', 'orientation', 'horizontal')
+legend boxoff
+set(h, 'fontSize', ftSize) 
 set(gca, 'fontSize', ftSize)
 
 

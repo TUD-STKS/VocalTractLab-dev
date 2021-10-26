0bb248e9991ed95aeabc48d6d2dbc1dad4ddf0c8
remi-kn <remi.blandin@gmail.com>
2021-10-26 15:28:48 +0200

    Add script to plot the acoustic field in simple cone

---
+++ validation/simple_cone/plot_simple_cone_field.m
@@ -0,0 +1,30 @@
+load p.txt
+load extracted_field/p_real_centerline.csv
+[xr, idxr] = sort(p_real_centerline(:,2));
+load extracted_field/p_imag_centerline.csv 
+[xi, idxi] = sort(p_imag_centerline(:,2));
+p_fem = p_real_centerline(idxr, 1) + 1i*p_imag_centerline(idxi, 1);
+x_fem = xi;
+
+x = repmat(8.5*(0:99)/99, 51, 1);
+y = (((1 + (0:99)/99)')*(2*(0:50)/50 - 1))';
+l = repmat(0.5 + 0.5*(0:99)/99, 51, 1);
+
+%fac = 10*sqrt(pi)/2;
+%fac = 0.96*10*sqrt(pi)/2;
+fac = 10^0.93;
+
+figure
+surf(x, y, p')
+
+figure
+plot(x_fem, log10(abs(p_fem)))
+hold on
+plot(x(25, :)/100, log10(fac*p'(25, :)))
+plot(x(25, :)/100, log10(p'(25, :)))
+
+figure
+plot(x_fem, abs(p_fem))
+hold on
+plot(x(25, :)/100, fac*p'(25, :))
+plot(x(25, :)/100, p'(25, :))

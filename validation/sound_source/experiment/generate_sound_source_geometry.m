4ab7057f82b2445527757f7aa4e2149553ed5c5b
Rémi Blandin <remi.blandin@tu-dresden.de>
2021-11-12 17:39:37 +0100

    Generate test geometries in the csv file format

--- validation/sound_source/experiment/generate_sound_source_geometry.m
+++ validation/sound_source/experiment/generate_sound_source_geometry.m
@@ -13,45 +13,125 @@ csvName = 'sound_source.csv';
 sep = ';';      % define separating character
 fid = fopen(csvName, 'w');
 %% generate square segment
+% % write x coordinates
+% fprintf(fid, '%f%s', 0., sep); % x coordinate of center
+% fprintf(fid, '%f%s', no(1), sep); % x coordinate of normal
+% fprintf(fid, '%f%s', 1., sep);    % entrance scaling factor
+% fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '\n')
+% 
+% % write y coordinates
+% fprintf(fid, '%f%s', 0., sep); % y coordinate of center
+% fprintf(fid, '%f%s', no(2), sep); % y coordinate of normal
+% fprintf(fid, '%f%s', 1., sep);    % exit scaling factor
+% fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s',sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '\n')
+% 
+% % write x coordinates
+% fprintf(fid, '%f%s', 10., sep); % x coordinate of center
+% fprintf(fid, '%f%s', no(1), sep); % x coordinate of normal
+% fprintf(fid, '%f%s', 1., sep);    % entrance scaling factor
+% fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
+% fprintf(fid, '\n')
+% 
+% % write y coordinates
+% fprintf(fid, '%f%s', 0., sep); % y coordinate of center
+% fprintf(fid, '%f%s', no(2), sep); % y coordinate of normal
+% fprintf(fid, '%f%s', 1., sep);    % exit scaling factor
+% fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s', sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '%f%s',sq/2, sep);  % y coordinate of contour
+% fprintf(fid, '\n')
+%% generate cone
+
+% generate contour coordinates
+nTheta = 100;
+theta = linspace(0, 2*pi, nTheta);
+cont = zeros(nTheta, 2);
+for t = 1:nTheta
+    cont(t, 1) = co(2)*cos(theta(t));
+    cont(t, 2) = co(2)*sin(theta(t));
+end
+
 % write x coordinates
 fprintf(fid, '%f%s', 0., sep); % x coordinate of center
 fprintf(fid, '%f%s', no(1), sep); % x coordinate of normal
-fprintf(fid, '%f%s', 1., sep);    % entrance scaling factor
-fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
-fprintf(fid, '\n')
+fprintf(fid, '%f%s', co(1)/co(2), sep);    % entrance scaling factor
+% x coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,1), sep);  
+end
+fprintf(fid, '\n');
 
 % write y coordinates
 fprintf(fid, '%f%s', 0., sep); % y coordinate of center
 fprintf(fid, '%f%s', no(2), sep); % y coordinate of normal
-fprintf(fid, '%f%s', 1., sep);    % exit scaling factor
-fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s',sq/2, sep);  % y coordinate of contour
-fprintf(fid, '\n')
+fprintf(fid, '%f%s', 1, sep);    % exit scaling factor
+% y coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,2), sep);  
+end
+fprintf(fid, '\n');
+
+%% Generate cylinder
+
+% generate contour coordinates
+nTheta = 100;
+theta = linspace(0, 2*pi, nTheta);
+cont = zeros(nTheta, 2);
+for t = 1:nTheta
+    cont(t, 1) = cy*cos(theta(t));
+    cont(t, 2) = cy*sin(theta(t));
+end
 
 % write x coordinates
-fprintf(fid, '%f%s', 10., sep); % x coordinate of center
+fprintf(fid, '%f%s', col, sep); % x coordinate of center
 fprintf(fid, '%f%s', no(1), sep); % x coordinate of normal
 fprintf(fid, '%f%s', 1., sep);    % entrance scaling factor
-fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % x coordinate of contour
-fprintf(fid, '%f%s', -sq/2, sep);  % x coordinate of contour
-fprintf(fid, '\n')
+% x coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,1), sep);  
+end
+fprintf(fid, '\n');
 
 % write y coordinates
 fprintf(fid, '%f%s', 0., sep); % y coordinate of center
 fprintf(fid, '%f%s', no(2), sep); % y coordinate of normal
-fprintf(fid, '%f%s', 1., sep);    % exit scaling factor
-fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s', -sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s', sq/2, sep);  % y coordinate of contour
-fprintf(fid, '%f%s',sq/2, sep);  % y coordinate of contour
-fprintf(fid, '\n')
-%% generate cone
+fprintf(fid, '%f%s', 1, sep);    % exit scaling factor
+% y coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,2), sep);  
+end
+fprintf(fid, '\n');
+
+% write x coordinates
+fprintf(fid, '%f%s', col + cyl, sep); % x coordinate of center
+fprintf(fid, '%f%s', no(1), sep); % x coordinate of normal
+fprintf(fid, '%f%s', 1., sep);    % entrance scaling factor
+% x coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,1), sep);  
+end
+fprintf(fid, '\n');
+
+% write y coordinates
+fprintf(fid, '%f%s', 0., sep); % y coordinate of center
+fprintf(fid, '%f%s', no(2), sep); % y coordinate of normal
+fprintf(fid, '%f%s', 1, sep);    % exit scaling factor
+% y coordinate of the contour
+for t = 1:nTheta
+    fprintf(fid, '%f%s', cont(t,2), sep);  
+end
+fprintf(fid, '\n');
 
 fclose(fid);

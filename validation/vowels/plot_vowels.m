5d92e011154eff9a0f23fdf6d8b53a9f7328256e
Rémi Blandin <remi.blandin@tu-dresden.de>
2021-11-15 18:14:32 +0100

    Update vowel simulations adding a computation 3 mm inside

--- validation/vowels/plot_vowels.m
+++ validation/vowels/plot_vowels.m
@@ -1,8 +1,8 @@
 
 area = 0.8;
-corrFac = (10^5);
+corrFac = (10^5)/area;
 
-vowels = {'a', 'u', 'i'};
+vowels = {'a', 'i', 'u'};
 types = {'_st', '', '_a'};
 typesSave = {'_straight', '_curved', '_curved_var_area'};
 load a_m.txt
@@ -15,16 +15,16 @@ colors = {'r', 'k', 'b', [0.2 0.6 0.4]};
 stringSave = 'save(''-mat'', ''vowels_MM.mat''';
 
 % loop over vowels
-for v = 1:3
+for v = 1:2
   load([vowels{v} '_m_fem.mat']);  % FEM 
   mod_MM = ones(nFreqs, 3);
   phase_MM = ones(nFreqs, 3);
-  for t = 1:3
+  for t = 2:2
     name = [vowels{v} '_m' types{t}];
     if exist([name '.txt'])
       load([name '.txt'])
-      eval(sprintf('mod_MM(:,%d) = %s(:,2);', t, name));
-      eval(sprintf('phase_MM(:,%d) = %s(:,3);', t, name));
+      eval(sprintf('mod_MM(:,%d) = %s(:,6);', t, name));
+      eval(sprintf('phase_MM(:,%d) = %s(:,7);', t, name));
       nameSave = [vowels{v} '_m' typesSave{t}];
       eval(sprintf('%s(:,1) = freqs;', nameSave));
       eval(sprintf('%s(:,2) = mod_MM(:,%d) * corrFac;', nameSave, t));
@@ -47,7 +47,7 @@ for v = 1:3
   end
 
   xlim([0 10000])
-  ylim([50 130])
+  ylim([70 150])
   xlabel('f (Hz)')
   ylabel('|H| (dB)')
 
@@ -57,7 +57,7 @@ for v = 1:3
   subplot(2,1,2)
   hold on
   plot(f, -angle(H), 'linewidth', lineSize, 'color', colors{1})
-  for t = 1:3
+  for t = 2:2
     plot(freqs, phase_MM(:,t), 'linewidth', lineSize, 'color', colors{t+1})
   end
 

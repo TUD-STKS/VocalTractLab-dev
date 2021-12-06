9959b3e68fc17154ae3303c9783ecb93cba60951
Simon Stone <sipreuss@gmail.com>
2021-12-06 15:12:41 +0100

    Adding an OS X build script and workflow

---
+++ build/osx/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install dependencies and build VocalTractLab
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxWidgets-3.1.3.tar.bz2
+tar -xf wxWidgets-3.1.3.tar.bz2
+cd wxWidgets-3.1.3
+mkdir buildosx
+cd buildosx
+../configure --with-opengl
+make
+sudo make install
+mkdir ../../../../tmp
+cd ../../../../tmp
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target VocalTractLab
